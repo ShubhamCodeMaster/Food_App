aac62f79943250460ed15fec948e80383151314e
ShubhamCodeMaster <shubham4121@gmail.com>
2023-11-17 16:31:41 +0530

    new commit

---
+++ client/src/containers/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Login() {
+  return (
+    <div className="w-screen h-screen relative overflow-hidden flex">
+      {/*backgroud image */}
+      
+    </div>
+  )
+}
+
+export default Login
